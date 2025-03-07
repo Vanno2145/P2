a7fced4d077ec55b9429f36eeb92c043902976fa
Ivan Mlanarskiy <Ivan Mlanarskiy@DESKTOP-RANKIB6>
2025-03-07 10:27:32 +0200

    s

--- P2/Program.cs
+++ P2/Program.cs
@@ -1,104 +1,76 @@
 ﻿using System;
 using System.Threading;
-
-class Account
-{
-    private int balance;
-    private readonly object lockObj = new object();
-
-    public Account(int initialBalance)
-    {
-        balance = initialBalance;
-    }
-
-    public void Deposit(int amount)
-    {
-        lock (lockObj)
-        {
-            balance += amount;
-            Console.WriteLine($"Пополнение счета на {amount}. Текущий баланс: {balance}.");
-        }
-    }
-
-    public void Withdraw(int amount)
-    {
-        lock (lockObj)
-        {
-            if (balance >= amount)
-            {
-                balance -= amount;
-                Console.WriteLine($"Снятие со счета {amount}. Текущий баланс: {balance}.");
-            }
-            else
-            {
-                Console.WriteLine($"Не достаточно средств для снятия {amount}. Текущий баланс: {balance}.");
-            }
-        }
-    }
-
-    public int GetBalance()
-    {
-        lock (lockObj)
-        {
-            return balance;
-        }
-    }
-}
-
-class Client
-{
-    private static Random random = new Random();
-    private Account account;
-
-    public Client(Account account)
-    {
-        this.account = account;
-    }
-
-    public void Start()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            Thread.Sleep(random.Next(1000, 3000));
-            int amount = random.Next(1, 101);
-
-            if (random.Next(2) == 0)
-            {
-                account.Deposit(amount);
-            }
-            else
-            {
-                account.Withdraw(amount);
-            }
-        }
-    }
-}
+using System.Threading.Tasks;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Account account1 = new Account(1000);
-        Account account2 = new Account(1000);
-        Account account3 = new Account(1000);
+        int[] nums = new int[20];
+        Task task1 = new Task(Sum);
+        Task task2 = new Task(WNumbers);
+        Task task3 = new Task(() => RNumbers(nums));
 
-        Client client1 = new Client(account1);
-        Client client2 = new Client(account2);
-        Client client3 = new Client(account3);
+        task1.Start();
+        task1.Wait();
 
-        Thread thread1 = new Thread(new ThreadStart(client1.Start));
-        Thread thread2 = new Thread(new ThreadStart(client2.Start));
-        Thread thread3 = new Thread(new ThreadStart(client3.Start));
+        task2.Start();
+        task2.Wait();
 
-        thread1.Start();
-        thread2.Start();
-        thread3.Start();
+        task3.Start();
+        task3.Wait();
+    }
 
-        Thread.Sleep(30000);
+    static void Sum()
+    {
+        int sum = 0;
+        for (int i = 0; i < 100; i++)
+        {
+            sum += i;
+        }
+    }
 
-        Console.WriteLine("Программа завершена.");
-        Console.WriteLine($"Состояние счета клиента 1: {account1.GetBalance()}.");
-        Console.WriteLine($"Состояние счета клиента 2: {account2.GetBalance()}.");
-        Console.WriteLine($"Состояние счета клиента 3: {account3.GetBalance()}.");
+    public static void WNumbers()
+    {
+
+        int[] nums = new int[20];
+        Random rand = new Random();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = rand.Next(1, 101);
+        }
+        string path = "D:\\file.txt";
+        using (StreamWriter file = new StreamWriter(path))
+        {
+            file.WriteLine(string.Join("", nums));
+        }
+    }
+
+    public static void RNumbers(int[] nums)
+    {
+        string path = "D:\\file.txt";
+        using (StreamReader sr = new StreamReader(path))
+        {
+            string fileContent = sr.ReadToEnd();
+            nums = fileContent.Split(' ')
+                        .Select(int.Parse)
+                        .ToArray();
+        }
+
+
+        var binaryNumbers = nums.Select(num => Convert.ToString(num, 2)).ToArray();
+
+
+        string path2 = "D:\\file2.txt";
+        using (StreamWriter file = new StreamWriter(path2))
+        {
+            file.WriteLine(string.Join(" ", binaryNumbers));
+        }
+
+        foreach (var binary in binaryNumbers)
+        {
+            Console.WriteLine(binary);
+        }
     }
 }
